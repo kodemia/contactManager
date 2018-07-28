1c20b513dc0b80e2ecc7164c3270f0202cc3cb7c
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2018-07-28 17:56:12 -0500

    login buttons

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function editField(selectedField){
 	console.log("fhield")
 	$(selectedField).closest("span").siblings(".data-label, .form-group").toggleClass("d-none")
 }
+
+function showModal(modalId){
+	$(modalId).modal("show")
+}
